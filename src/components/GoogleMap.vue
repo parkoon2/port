a23731cfd52e705e13bb1b545c0420604fe84cdd
parkoon <par_koon@naver.com>
2019-11-06 14:38:25 +0900

    update: google map

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -40,14 +40,10 @@ export default {
         }
     },
     mounted() {
-        // At this point, the child GmapMap has been mounted, but
-        // its map has not been initialized.
-        // Therefore we need to write mapRef.$mapPromise.then(() => ...)
-        // this.geolocate()
-        var Y_point = 35.837143 // Y 좌표
-        var X_point = 128.558612 // X 좌표
-        var zoomLevel = 18 // 지도의 확대 레벨 : 숫자가 클수록 확대정도가 큼
-        var markerTitle = '대구광역시' // 현재 위치 마커에 마우스를 오버을때 나타나는 정보
+        var Y_point = 37.588308 // Y 좌표,
+        var X_point = 127.210574 // X 좌표
+        var zoomLevel = 16 // 지도의 확대 레벨 : 숫자가 클수록 확대정도가 큼
+        var markerTitle = '남양주시' // 현재 위치 마커에 마우스를 오버을때 나타나는 정보
         var markerMaxWidth = 300 // 마커를 클릭했을때 나타나는 말풍선의 최대 크기
 
         // 말풍선 내용
@@ -76,11 +72,10 @@ export default {
             maxWizzzdth: markerMaxWidth
         })
 
-        console.log('google', google)
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open(map, marker)
-        })
-        infowindow.open(map, marker)
+        // google.maps.event.addListener(marker, 'click', function() {
+        //     infowindow.open(map, marker)
+        // })
+        // infowindow.open(map, marker)
     }
 }
 </script>
